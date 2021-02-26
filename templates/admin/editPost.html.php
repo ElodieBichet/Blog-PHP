d50c7aed1a692427c8bd0a18dda48be962d894a9
Elodie Bichet <e_bichet@yahoo.fr>
2021-02-26 15:59:18 +0100

    Modal confirmation
    Add a modal confirmation to delete a post.

--- templates/admin/editPost.html.php
+++ templates/admin/editPost.html.php
@@ -7,7 +7,28 @@
 
         <button type="submit" name="update" class="btn btn-primary">Mettre à jour</button>
         <button type="submit" name="updateAsDraft" class="btn btn-secondary">Mettre à jour comme brouillon</button>
-        <button type="submit" name="delete" class="btn btn-danger">Supprimer</button>
+        <button type="button" name="delete" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#myModal">Supprimer</button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Suppression du post</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Cette action est irréversible. Souhaitez-vous supprimer définitivement le post <?= $post->id ?> ?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                    <button type="submit" name="delete" class="btn btn-primary">Supprimer le post</button>
+                </div>
+                </div>
+            </div>
+        </div>
 
     </form>
+
+
 </div>
