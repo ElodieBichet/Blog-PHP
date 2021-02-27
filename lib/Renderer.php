1bde0f9afdf73a96b5e3afe7fe34ac99cfb9d8b6
Elodie Bichet <e_bichet@yahoo.fr>
2021-02-27 10:17:53 +0100

    Update templating system
    Split main layout into 2 specific layouts: one for front, one for admin
    Simplify render function to match with it

--- lib/Renderer.php
+++ lib/Renderer.php
@@ -4,41 +4,40 @@ namespace App;
 
 class Renderer
 {
-    public static function render(string $type='front', string $path, array $variables=[])
+    public static function render(string $type='front', string $path = '404-error', array $variables=[])
     {
         // variables initialization
         $pageTitle = 'Page sans titre';
-        $bodyClass = 'container';
-        $pageSidebar = '';
-        $mainClass = '';
         $alert = '';
         
         extract($variables);
 
         // Force connection if an admin page is requested (temporary feature while waiting for a real authentication system)
-        if (($type=='admin') AND (!isset($_SESSION['connection']) OR $_SESSION['connection'] == false)) {
-            $type = 'front';
-            $path = 'login';
-            $pageTitle = 'Connexion à l\'admin';
+        if ($type=='admin')
+        {
+            if ( !isset($_SESSION['connection']) OR $_SESSION['connection'] == false )
+            {
+                $type = 'front';
+                $path = 'login';
+                $pageTitle = 'Connexion à l\'admin';
+            }
+            else
+            {
+                ob_start();
+                require('../templates/admin/inc/sidebar.html.php');
+                $pageSidebar = ob_get_clean();
+            }
         }
 
         ob_start();
         require('../templates/'.$type.'/inc/header.html.php');
         $pageHeader = ob_get_clean();
 
-
-        if ($type=='admin') {
-            $bodyClass = 'container-fluid p-0';
-            $mainClass = 'col-md-9 ms-sm-auto col-lg-10 px-md-4';
-            ob_start();
-            require('../templates/'.$type.'/inc/sidebar.html.php');
-            $pageSidebar = ob_get_clean();
-        }
         
         ob_start();
         require('../templates/' . $type . '/' . $path . '.html.php');
         $pageContent = ob_get_clean();
     
-        require('../templates/layout.html.php');
+        require('../templates/'.$type.'/layout.html.php');
     }
 }
