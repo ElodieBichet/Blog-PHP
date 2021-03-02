ab71faafbe24fefbfba442140b2f6758b545606a
Elodie Bichet <e_bichet@yahoo.fr>
2021-03-02 17:41:40 +0100

    Update date format in front posts list

--- templates/front/posts-list.html.php
+++ templates/front/posts-list.html.php
@@ -1,15 +1,15 @@
 
-<p>Liste des posts en front.</p>
+<p>Découvrez les dernières news.</p>
 
 <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
     <?php foreach ($posts as $post) : ?>
-    <div class="col">
+    <div class="col" id="post-<?= $post->id ?>">
         <div class="card h-100">
             <div class="card-header">
                 <h2 class="card-title"><?= $post->title ?></h2>
             </div>
             <div class="card-body">
-                <p class="card-subtitle mb-2 text-muted">Dernière mise à jour : <?= $post->last_update_date ?></hp>
+                <p class="card-subtitle mb-2 text-muted">Dernière mise à jour : <?= date('\l\e d/m/Y à H:i', strtotime($post->last_update_date)) ?></p>
                 <p class="card-text">
                     <?= $post->intro ?>
                 </p>
