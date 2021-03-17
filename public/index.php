417ec7a3338fbceaae6c3f1f0a4ce4b550fb1a40
Elodie Bichet <e_bichet@yahoo.fr>
2021-03-17 10:57:55 +0100

    Add error catching for dev context

--- public/index.php
+++ public/index.php
@@ -10,5 +10,6 @@ try
 catch (Throwable $e)
 {
     echo '<p style="text-align:center;font-size:1.4em;font-weight:bold;margin-top:2em;">Désolé, un erreur est survenue :-(</p>';
-    // var_dump($e);
+    // Uncomment in dev context :
+    // echo 'Erreur : '. $e->getMessage() .'<br>Fichier : '. $e->getFile() .'<br>Ligne : '. $e->getLine();
 }
