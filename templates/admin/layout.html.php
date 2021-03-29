78899ae72c6d30952a31f13e030148cb985acdcc
Elodie Bichet <e_bichet@yahoo.fr>
2021-03-29 16:57:53 +0200

    'back to the list' button
    Add a button to go back to the items list after delete, valid or reject action

--- templates/admin/layout.html.php
+++ templates/admin/layout.html.php
@@ -33,6 +33,9 @@
             
                     <h1 class="my-3 pb-3"><?= filter_var($pageTitle, FILTER_SANITIZE_STRING) ?></h1>
                     
+                    <?php if (count(array_intersect(['delete','reject','valid'],array_keys($_REQUEST))) >= 1 AND !empty($_REQUEST['controller'])) : ?>
+                        <a class="btn btn-light btn-sm mb-2 mt-0 border" href="index.php?admin&controller=<?= filter_var($_REQUEST['controller'], FILTER_SANITIZE_STRING) ?>&task=showList"><i class="fas fa-arrow-left"></i> Retour à la liste <i class="fas fa-list"></i></a>
+                    <?php endif; ?>
                     <?= filter_var($alert) ?>
                     <?= filter_var($pageContent) ?>
     
